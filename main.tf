05ba06d0eea050fbc07a89d3efa8352ff8744d6d
Golloumette <caroline.boucau59@gmail.com>
2026-03-06 14:31:40 +0100

    exercice 4 ok

--- main.tf
+++ main.tf
@@ -9,11 +9,18 @@ terraform {
 
 provider "docker" { host = "tcp://localhost:2375" }
 
+#reseau docker 
+
+resource "docker_network" "private_network" {
+  name = "mon_reseau_docker"
+} 
+#conteneur 1
 resource "docker_image" "nginx" {
   name         = var.image_docker
   keep_locally = true
 }
 
+#imade du conteneur 1
 resource "docker_container" "nginx" {
   name  = var.nom_container
   image = docker_image.nginx.image_id
@@ -21,15 +28,34 @@ resource "docker_container" "nginx" {
     internal = var.int_port
     external = var.ext_port
   }
-
   provisioner "local-exec" {
     command = "curl -s http://localhost:${var.ext_port} | grep 'Welcome'"
   }
+#accrocher le conteneur au reseau prive
+networks_advanced {
+  name = docker_network.private_network.name
 }
+
+}
+
+
+#conteneur 2
+resource "docker_image" "client" {
+  name = "appropriate/curl"
+}
+
+resource "docker_container" "client" {
+  name  = "conteneur_client"
+  image = docker_image.client.image_id
+
+  command = ["sh", "-c" ,"curl -s http://nginx:80 && sleep 60"]
+
+  networks_advanced {
+  name = docker_network.private_network.name
+ }
+}
+
 
 output "nginx_container_id" {
-  value = docker_image.nginx.image_id
+  value = docker_container.nginx.id
 }
-
-  
-
