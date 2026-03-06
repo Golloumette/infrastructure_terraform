2078b03bcaea7418cb9a9ab634b50d2d2bf71115
Golloumette <caroline.boucau59@gmail.com>
2026-03-06 11:17:56 +0100

    exercice 1

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "docker" { host = "tcp://localhost:2375" }
+
+resource "docker_image" "nginx" {
+  name         = var.image_docker
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+  name  = var.nom_container 
+  image = docker_image.nginx.image_id
+  ports {
+    external = var.ext_port
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "image_docker" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the server."
+  default     = "nginx:latest"
+},
+variable "nom_container" {
+    type        = string
+  description = "Nom du conteneur"
+  default     = "nginx-terraform"
+},
+variable "ext_port"{
+     type        = number
+  description = "Port expose"
+  default     = 8080
+},
+variable "int_port"{
+     type        = number
+  description = "Port interne"
+  default     = 80
+}
