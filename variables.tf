c4bc8f8375cda81ba61a1806b184ceb475b971c2
Golloumette <caroline.boucau59@gmail.com>
2026-03-06 16:20:39 +0100

    exercice 7 ok

--- variables.tf
+++ variables.tf
@@ -26,3 +26,36 @@ variable "serveur-nom" {
   type    = list(string)
   default = ["zeus", "poseidon", "hades"]
 }
+variable "machines" {
+    type  = list(object({
+        name = string
+        vCpu = number
+        disk_size = number
+        region = string
+    }
+    ))
+    default = [
+    {
+      name      = "zeus"
+      vCpu      = 2
+      disk_size = 20
+      region    = "eu-west-1"
+    }
+  ]
+  #vérifier les conditions
+   validation {
+    condition = (
+        # alltrue ou 
+        #list d'objet donc on boucle
+        #on fait un ternaire
+      alltrue([for v in var.machines[*].vCpu      : v >= 2 && v <= 64]) &&
+      alltrue([for d in var.machines[*].disk_size : d >= 20]) &&
+      alltrue([for r in var.machines[*].region    : contains(["eu-west-1", "us-east-1", "ap-southeast-1"], r)])
+    )
+    error_message = "Chaque machine doit avoir entre 2 et 64 vCPU, un disque >= 20 Go, et une région valide."
+  }
+}
+
+
+
+
