2181fb30ee0a6c520ac5c494449a264d6b578b04
Golloumette <caroline.boucau59@gmail.com>
2026-04-17 15:06:35 +0200

    exo 5 s3 bucket et texte-file

--- AWS/s3.tf
+++ AWS/s3.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket_versioning" "demo_bucket_versioning" {
 
 resource "aws_s3_object" "demo_object" {
   bucket = aws_s3_bucket.demo_bucket.id
-  key    = "hello-world.txt"
+  key    = "test-file.txt"
   source = "./test-file.txt"
   etag   = filemd5("./test-file.txt")
 }
