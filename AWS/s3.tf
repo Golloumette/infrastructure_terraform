23e1d3d6744bea23e59db0ef2bffd963f0e0edb5
Golloumette <caroline.boucau59@gmail.com>
2026-04-17 15:12:34 +0200

    EXO 6 rendre le bucket public

--- AWS/s3.tf
+++ AWS/s3.tf
@@ -16,3 +16,31 @@ resource "aws_s3_object" "demo_object" {
   source = "./test-file.txt"
   etag   = filemd5("./test-file.txt")
 }
+
+resource "aws_s3_bucket_public_access_block" "demo_bucket_public_access" {
+  bucket = aws_s3_bucket.demo_bucket.id
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "demo_bucket_policy" {
+  bucket = aws_s3_bucket.demo_bucket.id
+
+  depends_on = [aws_s3_bucket_public_access_block.demo_bucket_public_access]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.demo_bucket.arn}/*"
+      }
+    ]
+  })
+}
